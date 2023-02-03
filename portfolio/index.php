ee2cb9dd32591bca317844856a08b0636daf8dd9
fullstack developer <67885971+alicco-27@users.noreply.github.com>
2023-02-03 19:22:33 +0500

    Add files via upload

---
+++ portfolio/index.php
@@ -0,0 +1,213 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Abdumannonov Muzaffar</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/animation.css">
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+</head>
+<body>
+   <header>
+    <nav class="head">
+        <div class="con">
+            <div class="new">
+                <a class="navbar-brand" href="#">MuzaffaR <b>&#60;&sol;&#62;</b></a>
+                <div class="ham">
+                    <input type="checkbox" name="" id="" class="check">
+                    <ul class="menu-items">
+                        <li><a href="#works">My Works</a></li>
+                        <li><a href="#skills">My skills</a></li>
+                        <li><a href="#contacts">Contacts</a></li>
+                        <li><a href="#about_me">About Me</a></li>
+                    </ul>
+                    <div class="ham-menu">
+                        <span class="line line1"></span>
+                        <span class="line line2"></span>
+                        <span class="line line3"></span>
+                    </div>
+                </div>
+            </div>
+            <form class="form-inline">
+                <input class="form-control mr-sm-3 search" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success " type="submit">Search</button>
+            </form>
+            <div class="main-men">
+                <ul class="menuu">
+                    <li><a href="#works">My Works</a></li>
+                    <li><a href="#skills">My skills</a></li>
+                    <li><a href="#contacts">Contacts</a></li>
+                    <li><a href="#about_me">About Me</a></li>
+                </ul>
+            </div>
+        </div>
+      </nav>
+   </header>
+   <a href="/" class="up"><button class="btn"></button></a>
+
+<section class="window">
+    <div class="con">
+        <div class="back">
+            <div class="con">
+                <div class="my-foto"></div>
+                    <div class="my-info">
+                        <p class="title">Hello everybody!</p><br>
+                        <p class="main h1"><span>I'M</span>  Abdumannonov Muzaffar</p>
+                        <p class="info">My name is Muzaffar. I am 18 years old. My place of residence is the Fargona region of the Republic of Uzbekistan. I am a fullstack web developer. This is my personal portfolio site😊</p><br><br>
+                        <button class="btn">more about me</button>
+                    </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="works" class="projects">
+      <p class="title">Projects</p>   <br><br> 
+                 <div class="con">
+                    <div class="project-first">
+                        <div class="image"></div>
+                        <p class="name">PROJECT 1</p>  
+                        <p class="info">Lorem, sumenda cum quidem doloremque?</p> 
+                        <button class="btn">Soon *_*</button>
+                    </div>
+                    <div class="project-first">
+                      <div class="image"></div>
+                      <p class="name">PROJECT 2</p>  
+                      <p class="info">Lorem, sumenda cum quidem doloremque?</p> 
+                      <button class="btn">Soon *_*</button>
+                    </div>
+                    <div class="project-first">
+                      <div class="image"></div>
+                      <p class="name">PROJECT 3</p>  
+                      <p class="info">Lorem, sumenda cum quidem doloremque?</p> 
+                      <button class="btn">Soon *_*</button>
+                    </div>
+                    <div class="project-first">
+                        <div class="image"></div>
+                        <p class="name">PROJECT 4</p>  
+                        <p class="info">Lorem, sumenda cum quidem doloremque?</p> 
+                        <button class="btn">Soon *_*</button>
+                    </div>
+                    <div class="project-first">
+                      <div class="image"></div>
+                      <p class="name">PROJECT 5</p>  
+                      <p class="info">Lorem, sumenda cum quidem doloremque?</p> 
+                      <button class="btn">Soon *_*</button>
+                    </div>
+                    <div class="project-first">
+                      <div class="image"></div>
+                      <p class="name">PROJECT 6</p>  
+                      <p class="info">Lorem, sumenda cum quidem doloremque?</p> 
+                      <button class="btn">Soon *_*</button>
+                    </div>
+                </div>
+</section>
+
+<section id="about_me" class="about">
+    <p class="title">About me</p>  <br><br><br>  
+    <div class="con">
+        <div class="i">
+            <div class="fullname"><img class="my_info" src="img/person.png" alt="">&nbsp;<b>FullName:</b> &nbsp; Abdumannonov Muzaffar</div>
+            <div class="phone"><img class="my_info_phone" src="img/phone.png" alt="">&nbsp;<b>Phone:</b> &nbsp; +998(91)-679-67-96</div>
+            <div class="direction"><img class="my_info" src="img/work.png" alt="">&nbsp;<b>Direction:</b> &nbsp; FullStack Web Developer</div>
+            <div class="birthday"><img class="my_info" src="img/gift.png" alt="">&nbsp;<b>Birthday:</b> &nbsp; 06.27.2005</div>
+        </div>
+        <div class="im">
+            <div class="adress"><img class="my_info_location" src="img/location.png" alt="">&nbsp;<b>Location:</b> &nbsp; Uzbekistan/Fergana</div>
+            <div class="insterest"><img class="my_info" src="img/person.png" alt="">&nbsp;<b>Interests:</b> &nbsp; write code and social media management</div>
+            <div class="mail"><img class="my_info_email" src="img/email.png" alt="">&nbsp;<b>Mail:</b> &nbsp; yangimuzaffar@gmail.com</div>
+        </div> 
+    </div>  <br><br>
+       <div class="descr">
+        <p class="desc">I know Html, Css, JavaScript, PHP, Laravel and can work with Figma.</p>
+    
+        <p class="desc">I started learning programming in 2022 and learned web programming at a training center called "iTeach Study".</p>
+       </div>
+</section>
+
+<section id="skills" class="skills">
+    <p class="title">Skills</p>
+    <div class="con">
+        <div class="skill">
+            <div class="progress">
+                <div class="progress-bar html" role="progressbar" style="width: 85%;" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100"> HTML 85%</div>
+            </div><br>
+            <div class="progress">
+                <div class="progress-bar css" role="progressbar" style="width: 80%;" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"> Css, SASS, Bootstrap 80%</div>
+            </div><br>
+            <div class="progress">
+                <div class="progress-bar js" role="progressbar" style="width: 60%;" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"> JavaScript 60%</div>
+            </div><br>
+            <div class="progress">
+                <div class="progress-bar php" role="progressbar" style="width: 70%;" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100"> PHP, Laravel 70%</div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="contacts" class="contacts">
+    <div class="con">
+        <div class="left">
+            <p class="title">Contact me</p><br>
+            <p class="desc">Write your opinion about the site! <b>I will be very happy with it</b>. Write to one of my e-mail and social networks for <b>website development</b> services.</p>
+        </div>
+        <div class="right col-12 col-lg-8">
+            <form class='contactform' method='post'><input type='hidden' name='form-name' value='form 1' />
+                <div class="contactform">
+                    <div class="row">
+                        <div class="col-12 col-md-4 mb-3">
+                            <input type="text" name="name" class="name" placeholder="Your name....">
+                        </div>
+                        <div class="col-12 col-md-4 mb-3">
+                            <input type="email" name="email" class="email" placeholder="Your email....">
+                        </div>
+                        <div class="col-12 col-md-4 mb-3">
+                            <input type="text" name="subject" class="subject" placeholder="Your subject">
+                        </div>
+                        <div class="col-12">
+                            <textarea name="message" class="message mb-3" placeholder="Your message"></textarea>
+                            <button type="submit" class="btn btn-contact">Send Message</button>
+                        </div>
+                        <div class="col-12 form-message">
+                            <span class="output_message text-center font-weight-600 text-uppercase"></span>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</section>
+
+<section class="footer">
+    <div class="con col-200">
+        <div class="about">
+            <div class="left">
+                <p class="title ">Contacts</p><br>
+                <img width="25px" src="img/phone.png" alt="">&nbsp;&nbsp; <a rel="nofollow"  href="tel:998916796796" aria-label="998916796796" class="number">+998-(91)-679-67-96</a><br><br>
+                <img width="25px" src="img/location.png" alt="">&nbsp;&nbsp;  <a target="_blank" href="https://goo.gl/maps/hKNnuBBFZKGDmedA8" class="location">Fergana, Uzbekistan</a><br><br>
+                <img width="25px" src="img/email.png" alt="">&nbsp;&nbsp; <a target="_blank" class="email fa fa-email" aria-hidden="true" href="mailto:yangimuzaffar@gmail.com?subject = Feedback&body = Message">yangimuzaffar@gmail.com</a>
+            </div>
+            <div class="right">
+                <p class="title ">Social Media</p><br>
+                <div class="images">
+                <img width="30px" src="img/telegram.png" alt="">&nbsp;&nbsp; <a  target="_blank" class="tg" href="https://t.me/Alicco_27">Telegram</a><br><br>
+                <img width="30px" src="img/youtube.png" alt="">&nbsp;&nbsp; <a  target="_blank" class="yt" href="https://www.youtube.com/@Alicco_27">YouTube</a><br><br>
+                <img width="30px" src="img/instagram.png" alt="">&nbsp; <a  target="_blank" class="ins" href="https://www.instagram.com/alicco_27/">Instagram</a>
+                </div>
+            </div>
+        </div>
+    </div>   
+</section>
+
+<script src="wow.js"></script>
+<script src="/examples/libs/ui-components/itc-slider/itc-slider.js" defer></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
